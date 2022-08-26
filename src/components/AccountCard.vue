214b675cab67f5fb50e62f45c927562abbd2f318
Guilherme Mazur <jjguilh@gmail.com>
2022-08-26 08:18:11 -0300

    feat: Main page register buttons working

--- src/components/AccountCard.vue
+++ src/components/AccountCard.vue
@@ -2,7 +2,7 @@
     <router-link to="/login" v-if='!$store.state.user' class="w-24 md:w-40 bg-blue-500 rounded h-12 py-3">Entrar</router-link>
     <div v-else class="w-28 md:w-40">
         <p class="text-xl">{{ $store.state.user.name }}</p>
-        <button @click="logout">Logout</button>
+        <button @click="logout" class="bg-red-500 p-1 text-sm text-white">Logout <img class="inline align-middle w-3" src="../assets/logout-svgrepo-com.svg"></button>
     </div>
 </template>
 <script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -13,27 +13,37 @@ export default {
       useGrouping: false
     })},
     getTime(){
-      const today = new Date("2022-08-25T20:33:32");
-      // var date = today.getFullYear()+'-'+this.twoDigits(today.getMonth()+1)+'-'+this.twoDigits(today.getDate());
-      // var time = this.twoDigits(today.getHours()) + ":" + this.twoDigits(today.getMinutes()) + ":" + this.twoDigits(today.getSeconds());
-      // var dateTime = date+'T'+time;
-      console.log(today.getHours());
-    }
+      const today = new Date();
+      let date = today.getFullYear()+'-'+this.twoDigits(today.getMonth()+1)+'-'+this.twoDigits(today.getDate());
+      let time = this.twoDigits(today.getHours()) + ":" + this.twoDigits(today.getMinutes()) + ":" + this.twoDigits(today.getSeconds());
+      let dateTime = date+'T'+time;
+      return dateTime;
+    },
+    recordArrive(){
+      axios.post('http://localhost:8000/api/timerecorder', {"tipo": "entrada", "timeLog": this.getTime(), "user_id": this.$store.state.user.id}).then(r=>{alert("Informação registrada com sucesso."); reload()}).catch(e=>alert(e))
+    },
+    recordExit(){
+      axios.post('http://localhost:8000/api/timerecorder', {"tipo": "saida", "timeLog": this.getTime(), "user_id": this.$store.state.user.id}).then(r=>{alert("Informação registrada com sucesso."); reload()}).catch(e=>alert(e))
+    },
+    test(){console.log(this.timeLogs)}
   },
-  mounted() {
-    if (this.$store.state.user != ''){
-    
-    } else if(localStorage.getItem('user')!='') {
+  async mounted() {
+    if(localStorage.getItem('user') && !this.$store.state.user) {
       this.$store.commit('saveUser', JSON.parse(localStorage.getItem('user')));
-      router.push('/')
+    };
+    if(this.$store.state.user){
+      let response = await axios.get(`http://localhost:8000/api/getlogs/${this.$store.state.user.id}`);
+      this.timeLogs = response.data
     }
   },
 }
 </script>
 
 <template lang="">
-    <div>
-        <Button class="mt-44" @click="getTime">aaaaaa</Button>
+    <div class="bg-blue-100 h-screen border pt-36 flex justify-around">
+        <button class="w-32 h-14 bg-green-500 text-black" @click="recordArrive">Registrar Chegada</button>
+        <button class="w-32 h-14 bg-red-500 text-black" @click="recordExit">Registrar Saída</button>
+        <!-- <button @click="test">aaaaaaa</button> -->
     </div>
 </template>
 
